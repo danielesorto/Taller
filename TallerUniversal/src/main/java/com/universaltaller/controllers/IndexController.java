e602b44b7a2a6b4609476af0579a55afc2bbcd66
Daniel Sorto <danielesorto@gmail.com>
2023-01-25 15:10:49 -0600

    adicion de template LTE

--- TallerUniversal/src/main/java/com/universaltaller/controllers/IndexController.java
+++ TallerUniversal/src/main/java/com/universaltaller/controllers/IndexController.java
@@ -1,8 +1,12 @@
 package com.universaltaller.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
-
+	@GetMapping({"/","/index","/home"})
+	public String index() {
+		return "index";
+	}
 }
